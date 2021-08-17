4ed56835a11f5ded54f1e166117baf4f772ef9c7
DumisanDhlamini <77558452+dhlaminitech@users.noreply.github.com>
2021-08-17 15:15:40 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html?>
+
+<html>
+        <head>
+            
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+            <link rel="stylesheet" type="text/css" href="assets/style/queries.css" />
+            <link rel="stylesheet" type="text/css" href="Assets/view/1.3 grid.css">
+            <style> body {background-size: cover; 
+                            background-image: url("assets/images/co.png"); 
+                            background-repeat: no-repeat;
+                            } </style>
+        </head>
+
+    <body>
+                            
+            <div class="thombe"> 
+            
+                 <img src="assets/images/Logo2.png" alt="SnowBox  Logo">
+                
+            </div>
+
+            <div class="landingBtn">
+
+                <div class="presa">
+                <a href="login.php" class="presaLogin">Sign In</a> <a href="register.php" class="presaRegister">Sign Up Here!</a>
+                        </div>
+            </div>
+
+                            <div class="vid">
+            <video controls src="Assets/video/Meddison -  Vocal Cord Testing.mp4">
+                            </div>
+    
+        </body>
+                            
+<html>
